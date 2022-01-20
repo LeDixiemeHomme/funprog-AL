f6cc21a9ff4fe662cca286504733ccf735250b4c
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-20 04:27:19 +0100

    fix(linter): working write in json file

--- src/main/scala/funprog/Main.scala
+++ src/main/scala/funprog/Main.scala
@@ -8,35 +8,6 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 
 object Main extends App {
-  //  val lawnmower = Lawnmower(
-  //    position = Coordinate(1, 1),
-  //    direction = Orientation.N
-  //  )
-  //
-  //  val coordinate: Coordinate = Coordinate(x = 1, y = 2)
-  //
-  //  val coo = JSONWriter.Coordinate
-  //
-  //  val json = coo.jsonWritesCoordinate
-  //
-  //  print(json.writes(coo(1,2)))
-  //
-  //
-  //  val lawnmowers: List[Lawnmower] = List(lawnmower)
-  //  val lawn: Lawn = Lawn(xMax = 5, yMax = 5, listOfLawnmower = lawnmowers)
-  ////  println(lawn.quartering)
-  //  lawnmower.move()
-  //
-  //  def timed[A](block: => A): A = {
-  ////    val t0 = System.currentTimeMillis()
-  //    val result = block
-  ////    println("took" + (System.currentTimeMillis() - t0))
-  //    result
-  //  }
-  //
-  //  val source = timed(scala.io.Source.fromFile("./src/main/resources/input.txt"))
-  //  val lines = timed(source.getLines())
-  //  print(lines)
 
   val mowers = ParserRefacto.execute()
 
@@ -46,12 +17,11 @@ object Main extends App {
   )
 
   val resJson = Result.jsonWrites.writes(o = result)
+
   val format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
   val now = format.format(Calendar.getInstance().getTime)
-  val f = File("src/main/resources/" + now + ".json") // using constructor
 
-  // pour ajouter du contenu dans un fichier ligne par ligne
-  f.createIfNotExists()
-    .appendLines(Json.prettyPrint(resJson)) // on ajoute 2 nouvelles lignes
+  val f = File("src/main/resources/" + now + ".json")
+  f.createIfNotExists().appendLines(Json.prettyPrint(resJson))
 
 }
