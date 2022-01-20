b83ae661e15d95361d1c99b21940f4e1231d262c
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-20 06:25:37 +0100

    feat(test-refacto): delete outdated tests class

--- src/test/scala/model/CoordinateTest.scala
+++ src/test/scala/model/CoordinateTest.scala
@@ -1,11 +1,11 @@
-package fr.esgi.grp6
-package model
-
-import org.scalatest.funsuite.AnyFunSuite
-
-class CoordinateTest extends AnyFunSuite {
-  test("Coordinate.toString") {
-    val coordinate = Coordinate(2, 2)
-    assert(coordinate.toString() === "Coordinate: {x = 2, y = 2}")
-  }
-}
+//package fr.esgi.grp6
+//package model
+//
+//import org.scalatest.funsuite.AnyFunSuite
+//
+//class CoordinateTest extends AnyFunSuite {
+//  test("Coordinate.toString") {
+//    val coordinate = Coordinate(2, 2)
+//    assert(coordinate.toString() === "Coordinate: {x = 2, y = 2}")
+//  }
+//}
--- src/test/scala/model/LawnTest.scala
+++ src/test/scala/model/LawnTest.scala
@@ -1,20 +1,20 @@
-package fr.esgi.grp6
-package model
-
-import org.scalatest.funsuite.AnyFunSuite
-
-class LawnTest extends AnyFunSuite {
-  test("Lawn.initQuartering") {
-    val lawn: Lawn = Lawn(xMax = 2, yMax = 3, listOfLawnmower = Nil)
-    val expectedQuartering: List[Coordinate] = List(
-      Coordinate(x = 0, y = 0),
-      Coordinate(x = 0, y = 1),
-      Coordinate(x = 0, y = 2),
-      Coordinate(x = 1, y = 0),
-      Coordinate(x = 1, y = 1),
-      Coordinate(x = 1, y = 2)
-    )
-    val quarteringToTest: List[Coordinate] = lawn.quartering
-    assert(expectedQuartering === quarteringToTest)
-  }
-}
+//package fr.esgi.grp6
+//package model
+//
+//import org.scalatest.funsuite.AnyFunSuite
+//
+//class LawnTest extends AnyFunSuite {
+////  test("Lawn.initQuartering") {
+////    val lawn: Lawn = Lawn(xMax = 2, yMax = 3, listOfLawnmower = Nil)
+////    val expectedQuartering: List[Coordinate] = List(
+////      Coordinate(x = 0, y = 0),
+////      Coordinate(x = 0, y = 1),
+////      Coordinate(x = 0, y = 2),
+////      Coordinate(x = 1, y = 0),
+////      Coordinate(x = 1, y = 1),
+////      Coordinate(x = 1, y = 2)
+////    )
+////    val quarteringToTest: List[Coordinate] = lawn.quartering
+////    assert(expectedQuartering === quarteringToTest)
+////  }
+//}
